664f0ccb4deda54b1364e967900eaa383ce53ec7
skyfroger <skyfroger@gmail.com>
2018-02-22 20:48:55 +0300

    Task solved

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for(var i = 0; i < preferences.length; i++){
+    index = i - 1; // получаем "реальный" индекс в массиве
+    if(preferences[index] == i){
+      continue; // пропускаем элементы указывающие сами на себя
+    }
+    middle = preferences[index]; // промежуточный элемент
+    last = preferences[middle - 1];
+    if(i == preferences[last - 1]){
+      count += 1;
+    }
+  }
+  return Math.floor((count + 1) / 3);
 };
